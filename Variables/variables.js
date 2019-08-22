eb984f79fe781812101f603de032f11086634b43
swayanshu <panda_swayanshu@yahoo.in>
2019-08-23 00:03:45 +0530

    primitive variables

---
+++ Variables/variables.js
@@ -0,0 +1,50 @@
+
+//Strings
+
+var myFirstVariable = 20
+var myName = "My name is Swayanshu."
+var myEmail = "swayanshu@email.com"
+
+var myParagraph = "This is the first line." + "This is the second line."
+
+alert(myFirstVariable)
+alert(myName)
+alert(myEmail)
+alert(myParagraph)
+
+//Numbers
+
+var score = 20
+var bigNumber = 32456789
+var decimalNumber = 49.34567
+
+alert(score)
+alert(bigNumber)
+alert(decimalNumber)
+
+//Boolean True or False
+
+var isResisteredUser = true;
+var isPaidUser = false;
+var sunRisesInTheEast = true;
+
+alert(isResisteredUser)
+alert(isPaidUser)
+alert(sunRisesInTheEast)
+
+//undefined- If you dont know the value, you can decide it later 
+
+var iDontKnow;
+
+alert(iDontKnow)
+
+//nul - A variable which doesnt have a value is undefined in javascript 
+// you can keep the value of a variable as null and later assign a meaningfull value 
+
+var iWillHaveNullValue = null;
+
+alert(iWillHaveNullValue)
+
+// Empty variable - They are neither null nor undefined, just empty 
+
+var myEmptyVariable = '' ;
