e666a31482bb9e0455e27571193cdd5ae1b34f22
swayanshu <panda_swayanshu@yahoo.in>
2019-08-26 12:29:26 +0530

    Operators

---
+++ Programming-Basics/operators.js
@@ -0,0 +1,49 @@
+
+var a = 10
+var b = 5 
+var c = 3
+
+// alert(a+b)
+// alert(a-b+c)
+// alert(a*b)
+// alert(a/c)
+// alert(a%c)
+
+/* 
+alert(a += b)
+alert(a -= b)
+alert(a *= b)
+alert(a /= b)
+alert(a %= b) */
+
+var x = 10
+var y = 5
+var z = "Aditya"
+var w = "Kumar"
+
+/* alert(x == y)
+alert(x != y)
+
+y = x
+alert(x === y)
+
+x = "10"
+alert(x == y)
+alert(x === y) */
+
+alert(z == w)
+alert(z != w)
+
+var a = "Aditya"
+var b = 10 
+var c = false 
+var d = { }
+var e = []
+
+alert(typeof(a))
+alert(typeof(b))
+alert(typeof(c))
+alert(typeof(d))
+alert(typeof(e))   // Array's type is shown as object if typeof operator is used
+
+alert(Array.isArray(e))
