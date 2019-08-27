c3bfbc35e4e0783a2fd57a617a620a3dd3eeeda1
swayanshu <panda_swayanshu@yahoo.in>
2019-08-28 00:16:19 +0530

    Special metods

---
+++ Programming-Basics/specialfunctions.js
@@ -0,0 +1,9 @@
+var bigString = "This is a very big string"
+var aSubstring = bigString.slice(0,4)
+//alert(aSubstring)
+
+var vegitables = ["Potatoes","Tomatoes","Green Chilly"]
+//alert(typeof(vegitables.toString()))
+
+var todayDate = new Date(Date.now())
+alert(todayDate)
