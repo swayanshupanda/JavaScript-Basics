8f39d6580f9fcd67ca3914d91ad5999106eeeafa
swayanshu <panda_swayanshu@yahoo.in>
2019-08-29 01:28:43 +0530

    Constants

---
+++ ES6/constant.js
@@ -0,0 +1,13 @@
+const discountPercentage = 10
+let oldCustomer = true
+
+if(oldCustomer){
+    const discountPercentage = 20
+    console.log(discountPercentage)
+} else {
+    //doNothing...
+}
+
+//discountPercentage = 15
+
+console.log(discountPercentage)
